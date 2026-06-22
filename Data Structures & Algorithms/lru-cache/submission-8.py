4ed039e7f6f76cf97c93924f4ee573d48a96ec03
Zhi Ming Xu <95781012+zmx27@users.noreply.github.com>
2026-06-22 10:10:47 -0400

    Add: lru-cache - submission-8

---
+++ Data Structures & Algorithms/lru-cache/submission-8.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev, self.next = None, None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {} # map key to the node
+
+        # Left is before the LRU, right is before the MRU
+        self.left, self.right = Node(0,0), Node(0,0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+
+    def get(self, key: int) -> int:
+        # Find node, remove it, and append it to the right
+        if key in self.cache:
+            node = self.cache[key]
+            self.remove(node)
+            self.insert(node)
+            return node.val
+        return -1
+        
+
+    def put(self, key: int, value: int) -> None:
+        # Remove node if exists. Then append new node to end
+        if key in self.cache:
+            node = self.cache[key]
+            node.val = value
+            self.remove(node)
+            self.insert(node)
+        else: # Make new node and add it to cache if didn't exist
+            self.cache[key] = Node(key, value)
+            self.insert(self.cache[key])
+
+        # Overflow so remove lru
+        if self.cap < len(self.cache):
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+    # Remove node from the linked list
+    def remove(self, node):
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+    
+    # Insert at end before self.right
+    def insert(self, node):
+        prev = self.right.prev
+        nxt = self.right
+        prev.next = node
+        node.prev = prev
+        node.next = nxt
+        nxt.prev = node
+
+
+        
