f26fd7aba17ab34452e3ddb9c0aa795100442ff5
Zhi Ming Xu <95781012+zmx27@users.noreply.github.com>
2026-07-01 20:43:26 -0400

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # Monotonically decreasing queue
+        q = deque() # Tracks indices of elements
+        # From left to right, the values the indices represent are in decreasng order
+        output = []
+        l, r = 0, 0 # l is beginning of the window
+        while r < len(nums):
+            while q and nums[r] > nums[q[-1]]:
+                # if curr element is already larger than prev added values, just remove them
+                # they can't be a future max (reduces work)
+                # smaller values will not be considered again
+                q.pop()
+            q.append(r) # Curr element added behind element strictly greater than it
+
+            if l > q[0]: # Window advanced beyond oldest, can't use it anymore
+                q.popleft()
+                # Only pop once because only oldest element is stale and can't be considered
+
+            if (r+1) >= k: # the window we are considering has reached size k
+                output.append(nums[q[0]])
+                l += 1 # left pointer incremented only when window is size k
+            r += 1 # right pointer always advances
+        return output    
