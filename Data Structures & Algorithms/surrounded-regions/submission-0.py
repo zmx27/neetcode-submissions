ba47749eb15f6ef47031a883b207d860fee04b64
Zhi Ming Xu <95781012+zmx27@users.noreply.github.com>
2026-07-09 14:19:23 -0400

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS, COLS = len(board), len(board[0])
+        directions = [[1,0], [-1,0], [0, 1], [0,-1]]
+
+        def dfs(r, c):
+            if min(r,c) < 0 or r == ROWS or c == COLS or board[r][c] != "O":
+                return
+            
+            board[r][c] = "#"
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                dfs(nr,nc)
+
+        # Traverse for cells in left and right border
+        for r in range(ROWS):
+            dfs(r, 0)
+            dfs(r, COLS-1)
+        
+        # Traverse for cells in top and bottom border
+        for c in range(COLS):
+            dfs(0, c)
+            dfs(ROWS-1, c)
+        
+        # Traverse the grid again
+        # Unmark "#" back to "O"
+        # Capture the "O" as "X"
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                if board[r][c] == "#":
+                    board[r][c] = "O"
+
+        
