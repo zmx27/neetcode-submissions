c440841d44bc366b4d5a3aa17bc20e81c52e14e5
Zhi Ming Xu <95781012+zmx27@users.noreply.github.com>
2026-07-21 16:58:07 -0400

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = defaultdict(set) # cols[c] tracks seen ints in column c
+        rows = defaultdict(set) # rows[r] tracks seen ints in row r
+        square = defaultdict(set) # square[(r,c)] tracks ints seen in square (r//3, c//3)
+
+        for r in range(9):
+            for c in range(9):
+                val = board[r][c]
+                if val == ".":
+                    continue
+
+                if val in cols[c] or val in rows[r] or val in square[(r//3, c//3)]:
+                    return False
+                
+                rows[r].add(val)
+                cols[c].add(val)
+                square[(r//3, c//3)].add(val)
+        return True
